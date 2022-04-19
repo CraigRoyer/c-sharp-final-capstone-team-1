2672e538a1f175ade35c78884c58fd8c8a9c8e94
[D[D[D[D[D[DTyler[C[C[C[SeFirst Last): Tyler Sellers <tsellers170@gmail.com>
2022-04-18 20:47:21 -0400

    merge fix remove merge conflict markers

--- vue/src/services/PlotService.js
+++ vue/src/services/PlotService.js
@@ -12,28 +12,9 @@ export default {
     return axios.post(`/plot/create`, plot);
   }
   
-<<<<<<< HEAD
-  // submitForm(){
-  //   axios.post('/contact', this.form)
-  //        .then((res) =>
-
-
-  // create(plot) {
-  //   return http.post(`/plot/create`, plot);
-  // },
-
-  // update(plotId, plot) {
-  //   return http.put(`/plot/${plotId}`, plot);
-  // },
-
-  // delete(plotId) {
-  //   return http.delete(`/plot/${plotId}`);
-  // }
-=======
   update(plotId, plot) {
     return axios.put(`/plot/${plotId}`, plot);
   },
->>>>>>> 75c58319ccbd5719ce754f7d444d2b4e90887421
 
 
   // update(plotId, plot) {
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,16 +2,11 @@
   <div class ="home">
     <h1>My Top Garden Plot</h1>
     <plot-detail v-bind:plotId='$route.params.plotId'/>
-<<<<<<< HEAD
-    <!-- <add-plot-form v-bind:plotId='$route.params.plotId'>YOOOO</add-plot-form>> -->
-    <add-plot></add-plot>
-=======
 
     <h2> Create New Plot </h2>
       <router-link :to="{ name: 'CreatePlot' }"
       >Create New Plot</router-link>
 
->>>>>>> 75c58319ccbd5719ce754f7d444d2b4e90887421
   </div>
 </template>
 
@@ -22,10 +17,6 @@ import AddPlot from '../components/AddPlot';
 export default {
   components: {
     PlotDetail,
-<<<<<<< HEAD
-    AddPlot
-=======
->>>>>>> 75c58319ccbd5719ce754f7d444d2b4e90887421
   }
 };
 </script>
